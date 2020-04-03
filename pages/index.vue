d0cc04b14f29fa8feeec9f71c8b263d7a76ffbe6
David <david@skribble.com>
2020-04-03 13:20:28 +0200

    Nuxt.js setup

---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-layout
+    column
+    justify-center
+    align-center
+  >
+    <v-flex
+      xs12
+      sm8
+      md6
+    >
+      <div class="text-center">
+        <img src="/logo-skribble.svg" alt />
+      </div>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+import VuetifyLogo from '~/components/VuetifyLogo.vue'
+
+export default {
+  components: {
+    Logo,
+    VuetifyLogo
+  }
+}
+</script>
