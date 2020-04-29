ddef64bfeef6f95e312556e6c680e250a37d2c42
Tim Head <betatim@gmail.com>
2020-04-29 17:52:20 +0200

    Add explanatory text and NL link

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,19 @@
 <template>
   <main>
     <div class="zeugs">
-      <back-soon />
-      <small>Elektronische Signaturen und digitales Vertrauen sind brandaktuelle Themen. Wegen Unterhaltsarbeiten bei Swisscom ist der VideoIdent Service temporär offline. Bitte probieren Sie es später nochmals.</small>
+      <coming-soon />
+      <small>Wir arbeiten gemeinsam mit Swisscom Trust Services an einer Lösung für die QES-Identifikation. Die Plattform für Video-Identifikation, videoident.me, wird in den nächsten Tagen zur Verfügung stehen. Sobald der Service genutzt werden kann, informieren wir über <a href="https://share.hsforms.com/1OCy9Xqw6Sza1rbvGUaKnqQ309ws" target="_blank">unseren Newsletter</a>.
+<br><br>Bestehende Skribble-Nutzer werden wir zusätzlich per E-Mail informieren.</small>
     </div>
   </main>
 </template>
 
 <script>
-import BackSoon from '~/components/BackSoon.vue'
+import ComingSoon from '~/components/ComingSoon.vue'
 
 export default {
   components: {
-    BackSoon,
+    ComingSoon,
   },
 }
 </script>
